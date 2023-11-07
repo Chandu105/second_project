7f9896ac40b4170bc245862072e1385eaedbc48b
Chandu105 <bandaruchandana2001@gmail.com>
2023-11-07 19:41:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def chandana(request):
+    return HttpResponse('<h1><marquee>my name is chandana</marquee></h1>')
